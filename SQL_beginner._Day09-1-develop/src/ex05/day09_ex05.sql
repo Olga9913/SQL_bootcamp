d0bf2cec912188023a603740ef5af611897173d8
Olga9913 <iinchocolate13@gmail.com>
2023-06-14 00:22:36 +0300

    day09

---
+++ SQL_beginner._Day09-1-develop/src/ex05/day09_ex05.sql
@@ -0,0 +1,23 @@
+DROP FUNCTION fnc_persons_female(), fnc_persons_male();
+
+CREATE FUNCTION fnc_persons(IN pgender VARCHAR DEFAULT 'female')
+    RETURNS TABLE
+            (
+                id      BIGINT,
+                name    VARCHAR,
+                age     INTEGER,
+                gender  VARCHAR,
+                address VARCHAR
+            )
+AS
+$$
+SELECT *
+FROM person
+WHERE pgender = person.gender
+$$
+    LANGUAGE SQL;
+
+SELECT *
+FROM fnc_persons(pgender := 'male');
+SELECT *
+FROM fnc_persons();
